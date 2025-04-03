3ceeeb79674987fc7dc550b9198faf50aca54d7a
Per1ova <azzabimourad2@gmail.com>
2025-04-03 15:35:42 +0200

    Lösung Dirty Screen #3

--- lib/src/features/dirty_screen_3/presentation/dirty_screen_three.dart
+++ lib/src/features/dirty_screen_3/presentation/dirty_screen_three.dart
@@ -8,88 +8,51 @@ class DirtyScreenThree extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Dirty Screen #3"),
       ),
-      body: SingleChildScrollView(
+      body: const SingleChildScrollView(
         scrollDirection: Axis.vertical,
         child: Column(
           children: [
-            Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(12),
-                  color: Colors.orangeAccent),
-              margin: const EdgeInsets.all(4),
-              padding: const EdgeInsets.all(16.0),
-              child: const Row(
-                children: [
-                  Icon(Icons.warning),
-                  SizedBox(
-                    width: 8,
-                  ),
-                  Text(
-                    'Alert 1',
-                    style: TextStyle(fontSize: 20, color: Colors.white),
-                  )
-                ],
-              ),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(12),
-                  color: Colors.orangeAccent),
-              margin: const EdgeInsets.all(4),
-              padding: const EdgeInsets.all(16.0),
-              child: const Row(
-                children: [
-                  Icon(Icons.warning),
-                  SizedBox(
-                    width: 8,
-                  ),
-                  Text(
-                    'Alert 2',
-                    style: TextStyle(fontSize: 20, color: Colors.white),
-                  )
-                ],
-              ),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(12),
-                  color: Colors.orangeAccent),
-              margin: const EdgeInsets.all(4),
-              padding: const EdgeInsets.all(16.0),
-              child: const Row(
-                children: [
-                  Icon(Icons.warning),
-                  SizedBox(
-                    width: 8,
-                  ),
-                  Text(
-                    'Alert 3',
-                    style: TextStyle(fontSize: 20, color: Colors.white),
-                  )
-                ],
-              ),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(12),
-                  color: Colors.orangeAccent),
-              margin: const EdgeInsets.all(4),
-              padding: const EdgeInsets.all(16.0),
-              child: const Row(
-                children: [
-                  Icon(Icons.warning),
-                  SizedBox(
-                    width: 8,
-                  ),
-                  Text(
-                    'Alert 4',
-                    style: TextStyle(fontSize: 20, color: Colors.white),
-                  )
-                ],
-              ),
-            ),
+            AlertBox(alertNumber: 1),   
+            AlertBox(alertNumber: 2),
+            AlertBox(alertNumber: 3),
+            AlertBox(alertNumber: 4),
+            AlertBox(alertNumber: 5),
+            AlertBox(alertNumber: 6),
+            AlertBox(alertNumber: 7),       
+            
           ],
         ),
+      ),
+    );
+  }
+}
+
+class AlertBox extends StatelessWidget {
+  final int alertNumber;
+
+  const AlertBox({
+    super.key,
+    required this.alertNumber
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12), color: Colors.orangeAccent),
+      margin: const EdgeInsets.all(4),
+      padding: const EdgeInsets.all(16.0),
+      child:  Row(
+        children: [
+           const Icon(Icons.warning),
+          const SizedBox(
+            width: 8,
+          ),
+          Text(
+            'Alert $alertNumber',
+            style: const TextStyle(fontSize: 20, color: Colors.white),
+          )
+        ],
       ),
     );
   }
